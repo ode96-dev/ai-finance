19ae14ab15482f7809719107d6b8e099f5c600a6
ode96-dev <150020491+ode96-dev@users.noreply.github.com>
2026-03-14 23:56:49 +0300

    implementation of layout clerk signup signin page

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-50 py-12">
+      <div className="container mx-auto px-4 text-center text-gray-600">
+        <p>Made with Wealth in Mind</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
